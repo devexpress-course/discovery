d6ada6e3a52913c039a7d01740298c6a984b0f47
Salomão Batista <salomaum.batiata@gmail.com>
2023-05-10 18:43:03 -0300

    first commit

---
+++ src/test/java/devexpress/course/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package devexpress.course.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
